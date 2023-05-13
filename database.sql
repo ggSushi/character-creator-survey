4acdb0a05e445fe908cf60cede1392f76f978cb2
Glonel Dimapilis <gdimapilis@aol.com>
2023-05-13 15:13:31 -0500

    set up thre localhost:5001

--- database.sql
+++ database.sql
@@ -1,8 +1,11 @@
+-- Database name: character-creator-app
+
 
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
