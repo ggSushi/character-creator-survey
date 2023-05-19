f080b54073078087ea40583450b4fdd22b1df854
Glonel Dimapilis <gdimapilis@aol.com>
2023-05-18 19:59:31 -0500

    Got the description page to show character info to DOM.

--- src/components/CharacterListPage/CharacterSheet/CharacterSheetDescription.jsx
+++ src/components/CharacterListPage/CharacterSheet/CharacterSheetDescription.jsx
@@ -1,11 +1,25 @@
-
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function CharacterSheetDescription() {
+  const dispatch = useDispatch();
+  const characterInfo = useSelector(store => store.charReducers.characterInfo);
+  const charId = useSelector(store => store.charReducers.characterId);
+
+  const getCharInfo = () => {
+    console.log(`charId stats page`,charId)
+    dispatch({ type: 'FETCH_CHAR_INFO', payload: charId})
+  }
+
+  useEffect(() => {
+    getCharInfo();
+  }, [])
 
 
   return(
     <>
     Describe yoself
+    {JSON.stringify(characterInfo)}
     </>
   )
 }
