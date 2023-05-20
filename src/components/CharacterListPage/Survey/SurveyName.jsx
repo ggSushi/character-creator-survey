55a4b47c1f31a01e93a922524fbb872c7e7f1fd8
Glonel Dimapilis <gdimapilis@aol.com>
2023-05-20 12:35:11 -0500

    first page of the survey is finished, taking in user's inputs for character name and campaign, and then saving it in a reducer.

--- src/components/CharacterListPage/Survey/SurveyName.jsx
+++ src/components/CharacterListPage/Survey/SurveyName.jsx
@@ -1,18 +1,40 @@
-import {useSelector, useDispatch} from 'react-redux';
-import {useEffect} from 'react';
-import {useHistory} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function SurveyName() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const characterName = useSelector(store => store.charReducers.characterName);
+  const campaignName = useSelector(store => store.charReducers.campaignName);
 
 
+  const nextPage = () => {
+    history.push('/survey-page-2');
+  }
 
+  const handleCharNameChange = (event) => {
+    let action = { type: 'SET_CHARACTER_NAME', payload: event.target.value };
+    dispatch(action)
+  }
 
-  return(
+  const handlecampaignNameChange = (event) => {
+    dispatch({ type: 'SET_CAMPAIGN_NAME', payload: event.target.value })
+  }
+
+  return (
     <>
-    Wassup, peeps?! PUT YOUR NAME AND CAMPAIGN IN HERE!! WOOOO!
-    <input />
-    <input />
-    <button>Next Page</button>
+      <div>
+        What is your character's name?
+        <br />
+        <input onChange={handleCharNameChange} placeholder="e.g. Leo B. Tronx" />
+      </div>
+      <div>
+        What campaign is this character in? (optional).
+        <br />
+        <input onChange={handlecampaignNameChange} placeholder="e.g. Icronus Campaign" />
+      </div>
+      <button onClick={nextPage}>Next Page</button>
     </>
   )
 }
