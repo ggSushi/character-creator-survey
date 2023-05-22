4bccfaf84a58a3251af041411ab300633d6cda5b
Glonel Dimapilis <gdimapilis@aol.com>
2023-05-21 21:44:52 -0500

    added array of sorcererSkills to surveyClassSkills page and mapped those options as choices for the user to click. choices get stored into skillBonus reducer.

--- src/components/CharacterListPage/Survey/SurveyClassSkills.jsx
+++ src/components/CharacterListPage/Survey/SurveyClassSkills.jsx
@@ -1,6 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import {useState} from 'react';
+import { useState, useRef } from 'react';
+
+let skillCounter = 0;
 
 function SurveyClassSkills() {
   const history = useHistory();
@@ -11,17 +13,56 @@ function SurveyClassSkills() {
   let [skillAmount, setSkillAmount] = useState(2);
 
 
+  //* all proficiency options for available classes
+  const sorcererSkills =[
+    {
+      id: 1,
+      name: 'Arcana',
+      description: 'Knowledgable in the nature of magic.'
+    },
+    {
+      id: 2,
+      name: 'Deception',
+      description: 'Tricking your way through many a situation.'
+    },
+    {
+      id: 3,
+      name: 'Insight',
+      description: `Telling a person's true intentions.`
+    },
+    {
+      id: 4,
+      name: `Intimidation`,
+      description: `Really scaring the pants off of people.`
+    },
+    {
+      id: 5,
+      name: `Persuasion`,
+      description: `Knowing how to get on someone's good side.`
+    },
+    {
+      id: 6,
+      name: `Religion`,
+      description: `Knowledgable in the theology of the world you're in.`
+    }
+  ]
+
+
+
+  // end skill proficiencies
+
+
   const setClassSkills = (event) => {
-    console.log(`skillAmount: ${skillAmount}`);
-    console.log(`skillBonus: ${skillBonus.length}`);
-    console.log(event.target.value);
-    if (skillBonus.length <= skillAmount - 1) {
-      dispatch({type: 'SET_SKILL_PROF', payload: event.target.value});
+    skillCounter += 1;
+    if (skillCounter <= skillAmount) {
+      dispatch({ type: 'SET_SKILL_PROF', payload: event.target.value });
     } else {
-            alert(`Please choose only ${skillAmount} of skills.`);
+      alert(`Please choose only ${skillAmount} of skills.`);
+      skillCounter = 0;
       return;
     }
   }
+
 
   return (
     <>
@@ -31,17 +72,14 @@ function SurveyClassSkills() {
           <div>
             <u>Please choose {skillAmount} from the following:</u>
             <form>
-              <input name="skill-prof-class" type="checkbox" onChange={setClassSkills} value="arcana" /> Arcana - Knowledgable in the nature of magic.
-              <br />
-              <input name="skill-prof-class" type="checkbox" onChange={setClassSkills} value="deception" /> Deception - Tricking your way through many a situation.
-              <br />
-              <input name="skill-prof-class" type="checkbox" onChange={setClassSkills} value="insight" /> Insight - Telling a person's true intentions.
-              <br />
-              <input name="skill-prof-class" type="checkbox" onChange={setClassSkills} value="intimidation" /> Intimidation - Really scaring the pants off of people.
-              <br />
-              <input name="skill-prof-class" type="checkbox" onChange={setClassSkills} value="persuasion" /> Persuasion - Knowing how to get on someone's good side.
-              <br />
-              <input name="skill-prof-class" type="checkbox" onChange={setClassSkills} value="religion" /> Religion - Knowledgable in the theology of the world you're in.
+              {
+                sorcererSkills.map(skill => (
+                  <div key={skill.id}>
+                    <input name="skill-prof-class"  type="checkbox" onChange={setClassSkills} value={skill.name} /> <b>{skill.name}</b> - {skill.description}
+                  </div>
+                ))
+              }
+              <input type="submit" value="Next Page" />
             </form>
           </div>
         ) : (
