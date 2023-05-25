9bd50ebc2b07ca6b232d1d6bfd5175a76420ca37
Glonel Dimapilis <gdimapilis@aol.com>
2023-05-25 16:02:59 -0500

    set up a new component to hold the AbilityScores and started getting the Charactersheet to look cleaner with some styling.

---
+++ src/components/CharacterListPage/CharacterSheet/AbilityScores.jsx
@@ -0,0 +1,80 @@
+import './Charactersheet.css';
+
+function AbilityScores({info}) {
+
+
+  return (
+    <>
+      <div className="abilities">
+        <div className="strength">
+          Str: {info.str_score}
+          <br />
+          {
+            info.str_mod >= 0 ? (
+              <b>(+{info.str_mod})</b>
+            ) : (
+              <b>({info.str_mod})</b>
+            )
+          }
+        </div>
+        <div className="dexterity">
+          Dex: {info.dex_score}
+          <br />
+          {
+            info.dex_mod >= 0 ? (
+              <b>(+{info.dex_mod})</b>
+            ) : (
+              <b>({info.dex_mod})</b>
+            )
+          }
+        </div>
+        <div className="constitution">
+          Con: {info.con_score}
+          <br />
+          {
+            info.con_mod >= 0 ? (
+              <b>(+{info.con_mod})</b>
+            ) : (
+              <b>({info.con_mod})</b>
+            )
+          }
+        </div>
+        <div className="intelligence">
+          Int: {info.int_score}
+          <br />
+          {
+            info.int_mod >= 0 ? (
+              <b>(+{info.int_mod})</b>
+            ) : (
+              <b>({info.int_mod})</b>
+            )
+          }
+        </div>
+        <div className="wisdom">
+          Wis: {info.wis_score}
+          <br />
+          {
+            info.wis_mod >= 0 ? (
+              <b>(+{info.wis_mod})</b>
+            ) : (
+              <b>({info.wis_mod})</b>
+            )
+          }
+        </div>
+        <div className="charisma">
+          Cha: {info.cha_score}
+          <br />
+          {
+            info.cha_mod >= 0 ? (
+              <b>(+{info.cha_mod})</b>
+            ) : (
+              <b>({info.cha_mod})</b>
+            )
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AbilityScores;
--- src/components/CharacterListPage/CharacterSheet/CharacterSheetStats.jsx
+++ src/components/CharacterListPage/CharacterSheet/CharacterSheetStats.jsx
@@ -1,5 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import AbilityScores from './AbilityScores.jsx';
+import './Charactersheet.css';
 
 
 function CharacterSheetStats() {
@@ -12,23 +14,23 @@ function CharacterSheetStats() {
 
   //* Make GET request for User's characters
   const getCharInfo = () => {
-    console.log(`charId stats page`,charId)
-    dispatch({ type: 'FETCH_CHAR_INFO', payload: charId})
+    console.log(`charId stats page`, charId)
+    dispatch({ type: 'FETCH_CHAR_INFO', payload: charId })
   }
 
   //* GET languages known
   const getLanguages = () => {
-    dispatch({ type: 'FETCH_LANGUAGES', payload: charId})
+    dispatch({ type: 'FETCH_LANGUAGES', payload: charId })
   }
 
   //* GET class info
   const getRaceInfo = () => {
-    dispatch({ type: 'FETCH_CLASS_INFO', payload: charId})
+    dispatch({ type: 'FETCH_CLASS_INFO', payload: charId })
   }
 
   //* GET race info
   const getClassInfo = () => {
-    dispatch({ type: 'FETCH_RACE_INFO', payload: charId})
+    dispatch({ type: 'FETCH_RACE_INFO', payload: charId })
   }
 
   useEffect(() => {
@@ -36,12 +38,33 @@ function CharacterSheetStats() {
     getLanguages();
     getRaceInfo();
     getClassInfo();
-    // console.log(charId);
   }, [])
+
 
   return (
     <>
-      This is the character sheet: stats Page
+      <div className="char-info-basic">
+        {
+          characterInfo.map(info => (
+            <div>
+              <div key={info.id}>
+                <h2 className="name-header"><u>{info.name}</u></h2>
+                <b><i>{info.race_name} {info.class_name}</i></b> Lv 1
+                <br />
+                <b>Campaign:</b> <i>{info.campaign}</i>
+                <br />
+                <b>Hit Points:</b> {info.hit_point_max}
+                <br />
+                <b>Proficiency Bonus: +{info.proficiency_bonus}</b>
+              </div>
+              <AbilityScores info={info}/>
+              
+            </div>
+          ))
+        }
+
+      </div>
+
 
       {JSON.stringify(characterInfo)}
       {JSON.stringify(languages)}
